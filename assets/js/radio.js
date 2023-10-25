1e0073d8f4d6c14216c5c2bba2fdebd41a7742a0
Lukman350 <nulhakiml725@gmail.com>
2023-10-25 10:41:44 +0700

    change request headers

--- assets/js/radio.js
+++ assets/js/radio.js
@@ -33,24 +33,15 @@ const getRadio = async (allRadio) => {
 			data = await $.ajax({
 				url: `http://${radio.ip_address}${endpoint}`,
 				type: "GET",
-				dataType: "html",
 				beforeSend: function (xhr) {
 					xhr.setRequestHeader(
 						"Authorization",
-						`Basic ${btoa(`${auth.username}:${auth.password}`)}`
+						`Basic ${btoa(`${auth.username}:${auth.password}`)}`,
+						"Access-Control-Allow-Origin",
+						"*",
+						"Access-Control-Allow-Credentials",
+						"true"
 					);
-				},
-				headers: {
-					Accept: "*/*",
-					"Accept-Encoding": "gzip, deflate",
-					"Accept-Language": "en-US,en;q=0.9",
-					"Cache-Control": "max-age=0",
-					Connection: "keep-alive",
-					Host: radio.ip_address,
-					"If-Modified-Since": "Thu, 01 Jan 1970 00:00:00 GMT",
-					Referer: `http://${radio.ip_address}`,
-					"Access-Control-Allow-Origin": "*",
-					"Access-Control-Allow-Credentials": "true",
 				},
 				success: function (result) {
 					return result;
